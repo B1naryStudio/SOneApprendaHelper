37b4f211fc861a963533ef92c69ec1dcdfeed1ff
kirmir <kirmir@outlook.com>
2015-03-02 14:03:19 +0200

    Initial commit with the basic ASP.NET MVC application and view for editing settings.

---
+++ SOneApprendaHelper/Controllers/ApprendaSettingsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using SOneApprendaHelper.Models;
+
+namespace SOneApprendaHelper.Controllers
+{
+    public class ApprendaSettingsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ApprendaSettings settings)
+        {
+            if (ModelState.IsValid)
+            {
+                var value = JsonConvert.SerializeObject(settings);
+                var cookie = new HttpCookie("ApprendaSettings", value) { Expires = new DateTime(3000, 1, 1) };
+                ControllerContext.HttpContext.Response.Cookies.Add(cookie);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SOneApprendaHelper/Models/ApprendaSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOneApprendaHelper.Models
+{
+    public class ApprendaSettings
+    {
+        // d113c1aa-e2bc-e411-80c6-0050560115d4
+        [Required]
+        [RegularExpression(@"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}")]
+        [Display(Name = "Application ID")]
+        public string ApplicationId { get; set; }
+
+        // d313c1aa-e2bc-e411-80c6-0050560115d4
+        [Required]
+        [RegularExpression(@"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}")]
+        [Display(Name = "Application Version ID")]
+        public string ApplicationVersionId { get; set; }
+    }
+}
