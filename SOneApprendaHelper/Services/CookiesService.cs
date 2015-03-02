ec0d92cba8f9d07150e9fb42cbb557e3874772e9
kirmir <kirmir@outlook.com>
2015-03-02 23:50:38 +0200

    Fixed the default values for settings.

--- SOneApprendaHelper/Services/CookiesService.cs
+++ SOneApprendaHelper/Services/CookiesService.cs
@@ -7,20 +7,18 @@ namespace SOneApprendaHelper.Services
 {
     public class CookiesService
     {
-        public T Get<T>(HttpCookieCollection cookies, string key)
+        public T Get<T>(HttpCookieCollection cookies, string key) where T : new()
         {
-            var data = default(T);
-
             if (cookies.AllKeys.Contains(key))
             {
                 var cookie = cookies[key];
                 if (cookie != null && !string.IsNullOrEmpty(cookie.Value))
                 {
-                    data = JsonConvert.DeserializeObject<T>(cookie.Value);
+                    return JsonConvert.DeserializeObject<T>(cookie.Value);
                 }
             }
 
-            return data;
+            return new T();
         }
 
         public void Set<T>(HttpCookieCollection cookies, string key, T value, DateTime expires)
