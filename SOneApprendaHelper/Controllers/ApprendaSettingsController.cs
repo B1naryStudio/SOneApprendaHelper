31a5dff01d83336ff2c2e540e5cbae84c8327134
kirmir <kirmir@outlook.com>
2015-03-02 14:22:26 +0200

    Finished Edit settings feature.

--- SOneApprendaHelper/Controllers/ApprendaSettingsController.cs
+++ SOneApprendaHelper/Controllers/ApprendaSettingsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
@@ -8,10 +9,23 @@ namespace SOneApprendaHelper.Controllers
 {
     public class ApprendaSettingsController : Controller
     {
+        private const string APPRENDA_SETTINGS_COOKIES_KEY = "ApprendaSettings";
+
         [HttpGet]
         public ActionResult Edit()
         {
-            return View();
+            ApprendaSettings settings = null;
+
+            if (ControllerContext.HttpContext.Request.Cookies.AllKeys.Contains(APPRENDA_SETTINGS_COOKIES_KEY))
+            {
+                var cookies = ControllerContext.HttpContext.Request.Cookies[APPRENDA_SETTINGS_COOKIES_KEY];
+                if (cookies != null && !string.IsNullOrEmpty(cookies.Value))
+                {
+                    settings = JsonConvert.DeserializeObject<ApprendaSettings>(cookies.Value);
+                }
+            }
+
+            return View(settings);
         }
 
         [HttpPost]
@@ -20,7 +34,7 @@ namespace SOneApprendaHelper.Controllers
             if (ModelState.IsValid)
             {
                 var value = JsonConvert.SerializeObject(settings);
-                var cookie = new HttpCookie("ApprendaSettings", value) { Expires = new DateTime(3000, 1, 1) };
+                var cookie = new HttpCookie(APPRENDA_SETTINGS_COOKIES_KEY, value) { Expires = new DateTime(3000, 1, 1) };
                 ControllerContext.HttpContext.Response.Cookies.Add(cookie);
             }
 
